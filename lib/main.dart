ae97fac0ebd5aef898e69209a8b56f91fb1b2dd4
Harmanjit Singh <harmanjit140500@gmail.com>
2020-07-07 19:12:39 +0530

    all done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -10,13 +11,20 @@ void main() {
           centerTitle: true,
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: DiceChange(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class DiceChange extends StatefulWidget {
+  @override
+  _DiceChangeState createState() => _DiceChangeState();
+}
+
+class _DiceChangeState extends State<DiceChange> {
+  int left = 1;
+  int right = 3;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -24,19 +32,31 @@ class DicePage extends StatelessWidget {
         children: <Widget>[
           Expanded(
             child: FlatButton(
+              splashColor: Colors.red[100],
+              onPressed: () {
+                setState(() {
+                  left = Random().nextInt(6) + 1;
+                });
+              },
               padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
               child: Image(
                 colorBlendMode: BlendMode.darken,
-                image: AssetImage('images/dice1.png'),
+                image: AssetImage('images/dice$left.png'),
               ),
             ),
           ),
           Expanded(
             child: FlatButton(
+              splashColor: Colors.red[100],
+              onPressed: () {
+                setState(() {
+                  right = Random().nextInt(6) + 1;
+                });
+              },
               padding: EdgeInsets.symmetric(vertical: 0, horizontal: 10.0),
               child: Image(
                 colorBlendMode: BlendMode.darken,
-                image: AssetImage('images/dice2.png'),
+                image: AssetImage('images/dice$right.png'),
               ),
             ),
           ),
